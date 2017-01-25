e72355dc3a00badc420a841d074cabf2b4b224f4
gislibg <gisligudmundss@gmail.com>
2017-01-25 09:14:53 +0000

    pair klassi sem hedur utan um coordinates

---
+++ Pair.java
@@ -0,0 +1,24 @@
+package prog1;
+
+public class Pair {
+	
+	private final int x;
+    private final int y;
+
+    public Pair(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int geX() 
+    {
+    	return this.x;
+    }
+    
+    public int getY()
+    {
+    	return this.y;
+    }
+
+}
