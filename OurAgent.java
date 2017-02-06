7bb2b6c50d76956ad87b14d41fa4f1058c503307
gislibg <gisligudmundss@gmail.com>
2017-02-06 16:52:13 +0000

    blerg

--- OurAgent.java
+++ OurAgent.java
@@ -111,19 +111,19 @@ public class OurAgent implements Agent{
 		*/
 		
 		createEnviorment();
-		for(Map.Entry<Pair, State> env : environment.entrySet())
+		/*for(Map.Entry<Pair, State> env : environment.entrySet())
 		{
 			System.out.println("loc: " +  env.getValue().getLocation().getX() + ", " + env.getValue().getLocation().getY() + ", " + "isDirt: " + env.getValue().isDirt() + ", isEast: " + env.getValue().isEast() + ", isNorth: " + env.getValue().isNorth() + ", isSouth: " + env.getValue().isSouth() + ", isWest: " + env.getValue().isWest() + ", intial: " + env.getValue().isInitial() + ", isObsticle: " + env.getValue().isObsticle());
 		}
-		
+		*/
 		lastGoal = new Pair(home.getX(), home.getY());
+		Stack<Integer> path = new Stack<Integer>();
 		for(int i = 0; i < dirtsLeft; i++)
 		{
 			createSearchGraph();
-			Stack<Integer> path = new Stack<Integer>();
-			//path = graph.BFS(CoordsAndInts.coordinatesToInt(lastGoal.getX(), lastGoal.getY(), size));
+			path = graph.BFS(CoordsAndInts.coordinatesToInt(lastGoal.getX(), lastGoal.getY(), size));
 			System.out.println("lastgoal " + lastGoal.getX() + ", " + lastGoal.getY());
-			path = graph.DFS(CoordsAndInts.coordinatesToInt(lastGoal.getX(), lastGoal.getY(), size));
+			//path = graph.DFS(CoordsAndInts.coordinatesToInt(lastGoal.getX(), lastGoal.getY(), size));
 			System.out.println(path);
 			if(path == null)
 			{
@@ -133,24 +133,29 @@ public class OurAgent implements Agent{
 			else
 			{
 				finalPath.add(path);
-				System.out.println("Peek: " + path.get(0));
+				//System.out.println("Peek: " + path.get(0));
 				updateEnv(CoordsAndInts.intToCoord(path.get(0), size));
 			}
 		}
-		System.out.println(finalPath);
+		createSearchGraph();
+		environment.get(keys.get(CoordsAndInts.coordinatesToInt(home.getX(), home.getY(), size))).setDirt(true);
+		path = graph.BFS(CoordsAndInts.coordinatesToInt(lastGoal.getX(), lastGoal.getY(), size));
+		//path = graph.DFS(CoordsAndInts.coordinatesToInt(lastGoal.getX(), lastGoal.getY(), size));
+		finalPath.add(path);
+		System.out.println("final:" + finalPath);
 		
 		makePath();
 	}
 
 	private void updateEnv(Pair peek) { 
-		System.out.println("Pair peek: " + peek.getX() + ", " + peek.getY());
+		//System.out.println("Pair peek: " + peek.getX() + ", " + peek.getY());
 		lastGoal.setPair(peek.getX(), peek.getY());
 		//dirts.remove(Integer.toString(peek.getX()) + ", " + Integer.toString(peek.getY()));
 
-		for(Map.Entry<Pair, State> env : environment.entrySet())
+		/*for(Map.Entry<Pair, State> env : environment.entrySet())
 		{
 			System.out.println("loc: " +  env.getValue().getLocation().getX() + ", " + env.getValue().getLocation().getY() + ", " + "isDirt: " + env.getValue().isDirt() + ", isEast: " + env.getValue().isEast() + ", isNorth: " + env.getValue().isNorth() + ", isSouth: " + env.getValue().isSouth() + ", isWest: " + env.getValue().isWest() + ", intial: " + env.getValue().isInitial() + ", isObsticle: " + env.getValue().isObsticle());
-		}
+		}*/
 	}
 
 	private void createSearchGraph() {
@@ -228,30 +233,31 @@ public class OurAgent implements Agent{
 				if(dirts.containsKey(Integer.toString(currPos.getX()) + ", " + Integer.toString(currPos.getY())))
 				{
 					//...and suck it up if it is present
-					System.out.println("Dirt!");
+					//System.out.println("Dirt!");
 					listOfMoves.add("SUCK");
 					dirts.remove(Integer.toString(currPos.getX()) + ", " + Integer.toString(currPos.getY()));	
 				}		
-				
+				/*
 				System.out.println("****Skrifa ut dirts****");
 				for(Map.Entry<String, Pair> dirt : dirts.entrySet())
 				{					
 					System.out.println("dirt: " + dirt.getKey());
 				}
-				
+				*/
 				//System.out.println("CurrPos: " + currPos.getX() + ", " + currPos.getY());
 				
 				System.out.println("CurrPos: " + currPos.getX() + ", " + currPos.getY() + "\nDestPos: " + dest.getX() + ", " + dest.getY() + "\nOrientation " + currOrientation);
 
 				if(currPos.getX() < dest.getX())
 				{
+					System.out.println("cX < dX");
 					if(currOrientation == Orientation.WEST)
 					{
 						//snúa mér til hægri
 						currOrientation = Orientation.NORTH;
 						//adda turn right
 						listOfMoves.add("TURN_RIGHT");
-						System.out.println("I turned " + currOrientation);
+						//System.out.println("I turned " + currOrientation);
 					}
 					if(currOrientation == Orientation.NORTH)
 					{
@@ -259,7 +265,7 @@ public class OurAgent implements Agent{
 						currOrientation = Orientation.EAST;
 						//adda turn right
 						listOfMoves.add("TURN_RIGHT");
-						System.out.println("I turned " + currOrientation);
+						//System.out.println("I turned " + currOrientation);
 					}
 					if(currOrientation == Orientation.SOUTH)
 					{
@@ -267,19 +273,20 @@ public class OurAgent implements Agent{
 						currOrientation = Orientation.EAST;
 						//adda turn left
 						listOfMoves.add("TURN_LEFT");
-						System.out.println("I turned " + currOrientation);
+						//System.out.println("I turned " + currOrientation);
 					}
 					
 				}
 				else if(currPos.getX() > dest.getX())
 				{
+					System.out.println("cX > dX");
 					if(currOrientation == Orientation.EAST)
 					{
 						//snúa mér til vinstri
 						currOrientation = Orientation.NORTH;
 						//adda turn left
 						listOfMoves.add("TURN_LEFT");
-						System.out.println("I turned " + currOrientation);
+						//System.out.println("I turned " + currOrientation);
 					}
 					if(currOrientation == Orientation.NORTH)
 					{
@@ -287,7 +294,7 @@ public class OurAgent implements Agent{
 						currOrientation = Orientation.WEST;
 						//adda turn left
 						listOfMoves.add("TURN_LEFT");
-						System.out.println("I turned " + currOrientation);
+						//System.out.println("I turned " + currOrientation);
 					}
 					if(currOrientation == Orientation.SOUTH)
 					{
@@ -295,18 +302,19 @@ public class OurAgent implements Agent{
 						currOrientation = Orientation.EAST;
 						//adda turn right
 						listOfMoves.add("TURN_RIGHT");
-						System.out.println("I turned " + currOrientation);
+						//System.out.println("I turned " + currOrientation);
 					}
 				}
 				else if(currPos.getY() < dest.getY())
 				{
+					System.out.println("cY < dY");
 					if(currOrientation == Orientation.SOUTH)
 					{
 						//snúa mér til hægri
 						currOrientation = Orientation.WEST;
 						//adda turn right
 						listOfMoves.add("TURN_RIGHT");
-						System.out.println("I turned " + currOrientation);
+						//System.out.println("I turned " + currOrientation);
 					}
 					if(currOrientation == Orientation.WEST)
 					{
@@ -314,7 +322,7 @@ public class OurAgent implements Agent{
 						currOrientation = Orientation.NORTH;
 						//adda turn right
 						listOfMoves.add("TURN_RIGHT");
-						System.out.println("I turned " + currOrientation);
+						//System.out.println("I turned " + currOrientation);
 					}
 					if(currOrientation == Orientation.EAST)
 					{
@@ -322,18 +330,19 @@ public class OurAgent implements Agent{
 						currOrientation = Orientation.NORTH;
 						//adda turn left
 						listOfMoves.add("TURN_LEFT");
-						System.out.println("I turned " + currOrientation);
+						//System.out.println("I turned " + currOrientation);
 					}
 				}
 				else if(currPos.getY() > dest.getY())
 				{
+					System.out.println("cY > dY");
 					if(currOrientation == Orientation.NORTH)
 					{
 						//snúa mér til vinstri
 						currOrientation = Orientation.WEST;
 						//adda turn left
 						listOfMoves.add("TURN_LEFT");
-						System.out.println("I turned " + currOrientation);
+						//System.out.println("I turned " + currOrientation);
 					}
 					if(currOrientation == Orientation.WEST)
 					{
@@ -341,7 +350,7 @@ public class OurAgent implements Agent{
 						currOrientation = Orientation.SOUTH;
 						//adda turn left
 						listOfMoves.add("TURN_LEFT");
-						System.out.println("I turned " + currOrientation);
+						//System.out.println("I turned " + currOrientation);
 					}
 					if(currOrientation == Orientation.EAST)
 					{
@@ -349,21 +358,22 @@ public class OurAgent implements Agent{
 						currOrientation = Orientation.SOUTH;
 						//adda turn right
 						listOfMoves.add("TURN_RIGHT");
-						System.out.println("I turned " + currOrientation);
+						//System.out.println("I turned " + currOrientation);
 					}
 				}
 				
 				//adda go!
-				if(currPos.getX() != dest.getX() || currPos.getY() != dest.getY())
+				if(currPos.getX() == dest.getX() && currPos.getY() == dest.getY()) {}
+				else
 				{
-					System.out.println("GOO");
+					//System.out.println("GOO");
 					listOfMoves.add("GO");
 					if(currOrientation == Orientation.EAST) { currPos.setPair(currPos.getX()+1, currPos.getY()); }
 					if(currOrientation == Orientation.NORTH) { currPos.setPair(currPos.getX(), currPos.getY()+1); }
 					if(currOrientation == Orientation.WEST) { currPos.setPair(currPos.getX()-1, currPos.getY()); }
 					if(currOrientation == Orientation.SOUTH) { currPos.setPair(currPos.getX(), currPos.getY()-1); }
 				}
-				System.out.println("\n\n");
+				//System.out.println("\n\n");
 			}
 		}
 		listOfMoves.add("TURN_OFF");
@@ -372,14 +382,17 @@ public class OurAgent implements Agent{
 
 	@Override
 	public String nextAction(Collection<String> percepts) {
-		/*System.out.print("perceiving:");
+		System.out.print("perceiving:");
 		for(String percept:percepts) {
 			System.out.print("'" + percept + "', ");
 		}
+		/*
 		System.out.println("");
 		String[] actions = { "TURN_ON", "TURN_OFF", "TURN_RIGHT", "TURN_LEFT", "GO", "SUCK" };
 		return actions[random.nextInt(actions.length)];*/
-		return listOfMoves.poll();
+		String temp = listOfMoves.poll(); 
+		System.out.println("action: " + temp);
+		return temp;
 	}
 
 }
